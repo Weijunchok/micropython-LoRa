5a9f986de5464ed52ae87aaaa2540136999bae43
Weijunchok <58077462+Weijunchok@users.noreply.github.com>
2020-11-19 08:32:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-#import LoRaDuplexCallback
-#import LoRaPingPong
-#import LoRaSender
+
 from config_sample import *
 from machine import SPI
 from lora_send_rece import *
-#from mqttinit import *
 from sx127x import SX127x
 
 
@@ -14,8 +11,8 @@ device_spi = SPI(SPI.SPI_SOFT,mode=SPI.MODE_MASTER,baudrate = 10000000,
         mosi = device_config['mosi'],
         miso = device_config['miso'],
         cs0=device_config['ss'])
-#mqtt_init()
 lora = SX127x(device_spi, pins=device_config, parameters=lora_parameters)
 #print(device_config['sck'])
 print(device_spi)
 lora_send(lora)
+#lora_rece(lora)
